097ecfc8a358548e3186f0242baa58347e542014
mikibella <bellamichael@web.de>
2024-12-02 12:09:30 +0100

    day2 part1 solution

--- day2/src/bin/part1.rs
+++ day2/src/bin/part1.rs
@@ -1,5 +1,5 @@
 use day2::part1;
 fn main() {
     let input = include_str!("../input.txt");
-    println!("{:?}", part1::process());
+    println!("Solution: {:?}", part1::process(input));
 }
--- day2/src/part1.rs
+++ day2/src/part1.rs
@@ -1,12 +1,40 @@
-pub fn process() -> Result<i32, anyhow::Error> {
-    todo!("Implement part1");
+pub fn process(input: &str) -> usize {
+    let mut counter: usize = 0;
+    for line in input.lines() {
+        let numbers: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect();
+        // check if numbers are sorted and step is not bigger than 3
+        if !is_sorted_and_step_not_bigger_than_three(&numbers) {
+            continue;
+        }
+        counter += 1;
+    }
+
+    counter
 }
+
+fn is_sorted_and_step_not_bigger_than_three(vec: &[i32]) -> bool {
+    let is_increasing = vec.windows(2).all(|w| w[0] < w[1]);
+    let is_decreasing = vec.windows(2).all(|w| w[0] > w[1]);
+    let step_not_bigger_than_three = vec.windows(2).all(|w| (w[0] - w[1]).abs() <= 3);
+
+    (is_increasing || is_decreasing) && step_not_bigger_than_three
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_process() {
-        assert!(false);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!(process(input), 2);
     }
 }
